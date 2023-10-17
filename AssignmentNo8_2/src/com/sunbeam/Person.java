61117de689360a4b218bc9a758c4ec9b3ee00f46
RutujaHatgine <rutujahatgine2000@gmail.com>
2023-10-17 15:12:52 +0530

    Assignment8_2

---
+++ AssignmentNo8_2/src/com/sunbeam/Person.java
@@ -0,0 +1,21 @@
+package com.sunbeam;
+
+public class Person implements Displayable
+{
+	private String name;
+    private int age;
+
+    public Person(String name,int age) {
+        this.name = name;
+        this.age=age;
+    }
+    @Override
+    public void show() {
+        System.out.println("Person: " + name);
+        System.out.println("Person: "+age);
+    }
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+}
