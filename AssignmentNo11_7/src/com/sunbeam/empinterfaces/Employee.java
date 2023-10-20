4e97327b44dd27f63948b46cb3218d27b3db792e
RutujaHatgine <rutujahatgine2000@gmail.com>
2023-10-20 19:20:05 +0530

    Assignment No.11_7

---
+++ AssignmentNo11_7/src/com/sunbeam/empinterfaces/Employee.java
@@ -0,0 +1,20 @@
+package com.sunbeam.empinterfaces;
+import java.util.*;
+public interface Employee 
+{
+abstract double getSal();
+	
+	default double calcIncentives() 
+	{
+		return 0.0;
+	}
+	
+	static double calcTotalIncome (Employee arr[]) {
+		double totalIncome = 0.0;
+		for(Employee a : arr) {
+		double income = a.calcIncentives() + a.getSal();
+ 		totalIncome = totalIncome + income;
+		}
+		return totalIncome;
+	}
+}
